cacb97d1c7ae1e285e3f6e16ef68609f77f169ff
iqbalfaslih <iqbalfaslih1@gmail.com>
2026-02-06 15:10:38 +0700

    setup backend api

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.json({status: "ok"});
+});
+
+app.listen(5000, () => {
+    console.log("API running on http://localhost:5000");
+});
